3838797ad88349a70e2042c82e32c1deb0f1e25e
chethanlreddy <chethanlreddy@outlook.com>
2023-03-05 12:18:52 +0530

    added admin users to the django

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    '''Databse model for users in the system'''
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default = False)
+    
+    objects = UserProfileManager()
+
+    USERNAME_FIELD  = "email"
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        '''Retrives full name'''
+        return self.name
+    
+    def get_short_name(self):
+        '''Retrives short name'''
+        return self.name
+    
+    def __str__(self):
+        '''Returns string representation of the user'''
+        return self.email
 
 # Create your models here.
