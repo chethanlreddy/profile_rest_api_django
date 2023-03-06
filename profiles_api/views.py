c803a7225cf6949e371107fd43ee7b20a4746829
chethanlreddy <chethanlreddy@outlook.com>
2023-03-06 22:11:02 +0530

    addedd views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
+
+class HelloApiView(APIView):
+    '''FIrst api'''
+    def get(self,request,format=None):
+        '''returns a list of ari view'''
+        return Response({'message':'Hello'})
