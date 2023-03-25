c5c13fd0c0237875fc15933d6f26a1da12574fcb
chethanlreddy <chethanlreddy@outlook.com>
2023-03-25 15:18:07 +0530

    add view_set

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from profiles_api import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('hello_view_set',views.HelloViewset,basename='hello_view_set')
 
 urlpatterns = [
     path('hello_view/',views.HelloApiView.as_view()),
+    path('',include(router.urls))
 ]
+
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework import viewsets
 from . import serializers
 # Create your views here.
 
@@ -30,4 +31,39 @@ class HelloApiView(APIView):
         return Response({'message':'patch'})
     
     def delete(self,reqest,pk=None):
+        return Response({'message':'delete'})
+    
+class HelloViewset(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+    def list(self,request):
+        a_viewset = [
+            '''
+            api view set all function tide together 
+            create, update, delete, partial_delete
+            '''
+        ]
+        return Response({'message':a_viewset})
+    
+
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello{name}'
+            return Response(message)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+
+    def retrieve(self,request,pk=None):
+        return Response({'message':'get'})
+    
+
+    def update(self,request,pk=None):
+        return Response({'message':'post'})
+    
+    def partial_update(self,request,pk=None):
+        return Response({'message':'patch'})
+    
+    def destroy(self,request,pk=None):
         return Response({'message':'delete'})
